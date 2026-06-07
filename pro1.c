b404d7953abe85c329047d6750e25e806749600d
Mohamed0916 <mdabuhuraira110@gmail.com>
2026-06-07 13:57:01 +0000

    Uploaded all c_assess4

---
+++ pro1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    int a,count;
+    printf("Numbers 1to5: ");
+    for(a=1;a<=5;a++){
+        count=a;
+        printf("\n%d",count);
+    }
+    return 0;
+}
---
+++ pro2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    printf("Numbers 5to1:");
+    for(a=5;a>0;a--){
+        printf("\n%d",a);
+    }
+    return 0;
+}
